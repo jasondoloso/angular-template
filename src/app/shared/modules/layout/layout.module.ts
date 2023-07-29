6fa5f09469d2353d3b9b06196f0ba2f9bbb1593f
Jason Doloso <jdoloso@exist.com>
2023-07-29 12:33:56 +0800

    Added Basic Directories

---
+++ src/app/shared/modules/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseComponent } from './base/base.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    BaseComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
